b3d07ea7cb1dbf2c0c4292fa8ac42b4f657c56a6
Iago Sales <99757969+IagoSalesP@users.noreply.github.com>
2022-04-06 16:40:39 -0300

    Segunda aula

---
+++ page01.php
@@ -0,0 +1,35 @@
+<?php
+
+// criar uma variavel //
+// case sensitivy: A != a // 
+// camelCase:  Exemplo: let corDoCarro = "Azul"; //
+// Toda liguagem case sensitive, devemos por obrigação utilizar a "Notação camelCase" // 
+// Quando a liguagem não é case-sensitive "case-insensitive" vamos utilizar a "Notação Pascal" // 
+
+
+
+
+$carro = "Fusca";
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+     <?php echo $carro ?> <!-- Comando Echo: enviar para a tela/navegador  -->
+
+    <!-- Imagem aleátoria -->
+     <img src="https://picsum.photos/200/300" alt="Imagem Aleatória">
+
+     
+
+    
+</body>
+</html>
