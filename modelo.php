2505c9282c77c2673e722a8446b5dcff4b459502
Iago Sales <99757969+IagoSalesP@users.noreply.github.com>
2022-04-08 16:46:58 -0300

    Aula03

---
+++ modelo.php
@@ -0,0 +1,28 @@
+<?php
+
+// Inclui o cabeçalho da página // 
+// include: Se o arquivo não existe, a um warning (E_WARNING) é lançado mas sua aplicação continua funcionando //
+// require: Se o php não localizar o arquivo, um fatal error é lançado (E_COMPILE_ERROR). neste caso o script para.//
+require('_header.php');
+
+?>
+            <section>
+
+                <h2>Título da Página</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, exercitationem, repellat modi vel adipisci reprehenderit laboriosam rem dolore ipsa libero blanditiis vitae, illum voluptas? Voluptas tenetur facilis nostrum nam architecto!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque rerum vel similique quod modi id labore debitis, harum cupiditate. Ullam illum cum itaque vero inventore amet eum obcaecati harum nisi.</p>
+
+
+            </section>
+
+            <aside>
+
+                <h3>Lateral do Site</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores consequuntur quidem reprehenderit cumque ullam esse mollitia eius. Cum ab labore eligendi aliquid, dolor enim illum ratione consequuntur commodi cupiditate quis.</p>
+
+            </aside>
+<?php
+
+require('_footer.php');
+
+?>
